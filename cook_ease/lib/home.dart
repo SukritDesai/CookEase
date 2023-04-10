47a69868eab2c3cfd67811de3a8335f135d21505
Zenit <dragonfire5906@gmail.com>
2023-04-09 23:17:10 -0400

    big patch

--- cook_ease/lib/home.dart
+++ cook_ease/lib/home.dart
@@ -172,3 +172,36 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            // Navigate to second route when tapped.
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+    );
+  }
+}
