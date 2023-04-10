cbf7f1d9cd39d836ba944e8ffbec7cd762e1bd6c
Sukrit Desai <sukritdesai@gmail.com>
2023-04-09 20:28:12 -0400

    Changes by Ethan

--- cook_ease/lib/Responses.dart
+++ cook_ease/lib/Responses.dart
@@ -1,14 +1,37 @@
-import 'package:cook_ease/IngredientsList.dart';
+import 'dart:io';
+
+import 'IngredientsList.dart';
 import 'extra.dart';
-class Responses{
+
+// void main() {
+//   Responses r = Responses();
+//   r.writeToFile();
+// }
+main() {
+  Responses r = Responses();
+  r.writeToFile();
+}
+
+class Responses {
   static List<Ingredient?> ingredients3 = [];
   static List<String> selectedIntos = [];
   static List<Cuisine?> cuisines2 = [];
   static String diet = diets.first;
-  void printout(){
-    print(ingredients3);
-    print(selectedIntos);
-    print(cuisines2);
-    print(diet);
+  void writeToFile() {
+    final file = File("lib/backend/inputs.txt");
+    String result = "";
+    for (int i = 0; i < ingredients3.length; i++) {
+      if (i == ingredients3.length - 1) {
+        result += "${ingredients3[i]?.getName()}";
+      } else {
+        result += "${ingredients3[i]?.getName()},";
+      }
+    }
+    //file.writeAsString(result);
+
+    // var file = File("/backend/inputs.txt");
+    // var sink = file.openWrite();
+    // sink.write("$ingredients3\n$selectedIntos\n$cuisines2\n$diet");
+    // sink.close();
   }
 }
